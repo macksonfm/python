d90288465113f96a70e287bda2704051ca7bbcb3
Mackson Moreira <mackson.moreira@gmail.com>
2020-03-04 22:26:27 -0300

    Aula 01

---
+++ Aulas/basic.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+nome = 'Mackson'
+idade = 38
+altura = 1.66
+brasileiro = True
+
+# print('Olá meu nome eh', nome) # Primeiro
+# print('Ol[a meu nome eh{}'.format(nome)) # Segundo
+# print(f'Olá meu nome eh {nome}') # Terceiro
+
+# Vendo tipos de dados
+# print(type(nome))
+# print(type(idade))
+# print(type(altura))
+# print(type(brasileiro))
+
+#nome = input('Digite seu nome: ')
+#print(f'O nome do usuario eh {nome}')
+
+#num1 = int (input('Digite o primeiro numero: '))
+#num2 = int (input('Digite o segundo numero: '))
+#print(num1+num2)
+
+# Metodo de String
+#texto = 'esse eh um texto'
+#print(texto.upper())
+#print(texto.lower())
+#print(texto.capitalize())
+#print(texto.split(' '))
+
+
+
